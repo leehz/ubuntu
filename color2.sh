1e1831f92db11c6fa34bd0fa458fc7131eac221c
huaixiaoz <huaixiaoz@huaixiaoz.com>
2015-01-20 09:38:54 +0800

    fixed

--- color2.sh
+++ color2.sh
@@ -4,7 +4,7 @@ echo  -e -n "-------------------------------\n"
 for i in {30..37}
 do  
     for j in $(seq 00 07);
-    do echo -e -n "\e[${j};${i}m aaa\e[0m";
+    do echo -e -n "\e[${j};${i}m ${j} ${i}\e[0m";
     done;
     echo -n  -e "\n";
 done
@@ -16,7 +16,7 @@ echo  -e -n "-------------------------------\n"
 for i in $(seq 00 07); 
 do  
     for j in $(seq 30 37);
-    do echo -e -n "\e[${i};${j}m aaa\e[0m";
+    do echo -e -n "\e[${i};${j}m ${i} ${j}\e[0m";
     done;
     echo -n  -e "\n";
 done
@@ -29,7 +29,7 @@ do
     do 
         for k in {40..47};
         do
-            echo -e -n "\e[${i};${j};${k}m aaa\e[0m";
+            echo -e -n "\e[${i};${j};${k}m ${i}${j}${k} \e[0m";
         done;
         echo -n -e "\n";
     done;
