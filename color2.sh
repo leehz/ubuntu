9908544624448130610c1284ca5b108400815495
huaixiaoz <huaixiaoz@huaixiaoz.com>
2015-01-20 09:33:06 +0800

    color2.sh added

---
+++ color2.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo  -e -n "-------------------------------\n"
+for i in {30..37}
+do  
+    for j in $(seq 00 07);
+    do echo -e -n "\e[${j};${i}m aaa\e[0m";
+    done;
+    echo -n  -e "\n";
+done
+echo  -e -n "-------------------------------\n"
+
+
+
+echo  -e -n "-------------------------------\n"
+for i in $(seq 00 07); 
+do  
+    for j in $(seq 30 37);
+    do echo -e -n "\e[${i};${j}m aaa\e[0m";
+    done;
+    echo -n  -e "\n";
+done
+echo  -e -n "-------------------------------\n"
+
+echo  -e -n "-------------------------------\n"
+for i in $(seq 00 07); 
+do  
+    for j in $(seq 30 37);
+    do 
+        for k in {40..47};
+        do
+            echo -e -n "\e[${i};${j};${k}m aaa\e[0m";
+        done;
+        echo -n -e "\n";
+    done;
+echo  -e -n "-------------------------------\n"
+done
+echo  -e -n "-------------------------------\n"
