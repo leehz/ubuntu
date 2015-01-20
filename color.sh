2b09535fec03eb2531f95569c85699fd77a69dcc
hz <hz@huaixiaoz.com>
2015-01-19 18:50:04 -0800

    color.sh

---
+++ color.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo  -e -n "-----------------------------\n"
+for i in $(seq 31 37); 
+do  
+    for j in $(seq 01 07);
+    do echo -e -n "\e[${j};${i}m aaa\e[0m";
+    done;
+    echo -n  -e "\n";
+done
+echo  -e -n "-----------------------------\n"
+
+
+
+echo  -e -n "-----------------------------\n"
+for i in $(seq 01 07); 
+do  
+    for j in $(seq 31 37);
+    do echo -e -n "\e[${i};${j}m aaa\e[0m";
+    done;
+    echo -n  -e "\n";
+done
+echo  -e -n "-----------------------------\n"
