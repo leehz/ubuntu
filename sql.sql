758b1f9bc7214a1a0eb265b26729a5c127cf0f6e
huaixiaoz <huaixiaoz@huaixiaoz.com>
2015-02-12 20:22:54 +0800

    a sql for insert syntax

---
+++ sql.sql
@@ -0,0 +1,3 @@
+insert into resource_info (ResourceID, Type, ModelID,  PoolID,Username, Password, Name)
+
+ insert into resource_info (ResourceID, Type, ModelID,  PoolID,Username, Password, Name) values ( 5, 'KVM', 5, 1, 'ccmp', '123456', 'CCMP5');
